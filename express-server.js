efcd2cd99486a06f2473552ecb7424dc7600b7be
Misha Krasnonos <muzishutka7@gmail.com>
2021-11-09 00:34:53 +0200

    up

--- express-server.js
+++ express-server.js
@@ -1,73 +1,20 @@
 
 const express = require('express')
 const morgan = require('morgan')
-const got = require('got');
-const app = express()
-const {router} = require("./Router")
 require("dotenv").config()
+
 const e = process.env
-const cors = require('cors')
- 
-// app.use(express.json())
-// app.use(express.urlencoded({extended: true}))
-// app.use(express.static('public'))
+const app = express()
+const {postsRouter} = require("./routers/postsRouter")
+const PORT = e.PORT || 8083
+
+app.use(express.json())
 app.use(morgan('tiny'))
-app.use(cors())
-// app.use('/api', router)
-
-const PORT = e.PORT
-const baseURL = "http://api.weatherbit.io/v2.0/current/"
-
-app.get('/api/weather', async (req, res) => {
-    try {
-
-        const {latitude, longitude} = req.query
-
-        if (!latitude) {
-           return res.status(400).json({message: "Latitude parameter is mandatory-empty"})
-        }
-
-        if (!longitude) {
-           return res.status(400).json({message: "Longitude parameter is mandatory-empty"})
-        }
-    
-        const response = await got(baseURL, {searchParams: {
-            key: e.API_KEY,
-            lat: latitude,
-            lon: longitude,
-        },
-        responseType: 'json'
-    });
-
-        const [data] = response.body.data
-        const {temp, sunrise, weather: {description}, city_name} = data
-        res.json({ city_name, temp, description, sunrise})
-
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-}) 
-
-// Custom Middleware
-// app.use((req, res, next) => {
-//     console.log(`${req.method} ${req.originalUrl} ${new Date().toISOString()}`);
-//     next();
-// })
-
-// app.get('/home', (req, res) => {
-    //     res.send('get request')
-    // })
-// app.delete('/home', (req, res) => {
-//     res.send('delete reuest')
-// })
-
-// app.use((req, res) => {
-//     res.status(500).json({javascript: 'asdasd'})
-// })
+app.use('/api/posts', postsRouter)
 
 app.listen(PORT, (err) => {
     if (err) {
         console.error('Error at a server launch', err);
     }
-    console.log(`Server work at port ${PORT}`);
+    console.log(`Server work at port ${PORT}!`);
 })
---
+++ routers/postsRouter.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+const posts = [
+    {id: '1', topic: 'test1', test: 'test text1'},
+    {id: '2', topic: 'test2', test: 'test text2'},
+    {id: '3', topic: 'test3', test: 'test text3'},
+]
+
+// GET /api/posts => [...posts]
+router.get('/', (req, res) => {
+   res.json({posts, status: 'success'}) 
+})
+
+// GET /api/posts/<123> => {post with id 123}
+router.get('/:id', (req, res) => {
+    const [post] = posts.filter(it => it.id === req.params.id)
+    res.json({post, status: 'successsss'})
+})
+
+// GET /api/posts => [newPost, ...posts]
+router.post('/', (req, res) => {
+  const {topic, text} = req.body
+  posts.push({id: new Date.getTime().toString(), topic, text})
+  res.json({status: 'success'})
+})
+
+router.put('/:id', (req, res) => {
+    
+})
+
+router.delete('/:id', (req, res) => {
+    
+})
+
+module.exports = {postsRouter: router}
