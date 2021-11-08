721f1a4d6c0e17c16628073cce80ea3f90681938
Misha Krasnonos <muzishutka7@gmail.com>
2021-11-09 01:15:29 +0200

    up

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const posts = [
-    {id: '1', topic: 'test1', test: 'test text1'},
-    {id: '2', topic: 'test2', test: 'test text2'},
-    {id: '3', topic: 'test3', test: 'test text3'},
+let posts = [
+    {id: '1', topic: 'test1', text: 'test text1'},
+    {id: '2', topic: 'test2', text: 'test text2'},
+    {id: '3', topic: 'test3', text: 'test text3'},
 ]
 
 // GET /api/posts => [...posts]
@@ -13,23 +13,39 @@ router.get('/', (req, res) => {
 
 // GET /api/posts/<123> => {post with id 123}
 router.get('/:id', (req, res) => {
+  const {id} = req.params
     const [post] = posts.filter(it => it.id === req.params.id)
+
+if (!post) {
+ return res.status(400).json({ status: `don't have id ${id}`})
+}
     res.json({post, status: 'successsss'})
 })
 
-// GET /api/posts => [newPost, ...posts]
+// POST /api/posts => [changePost, ...posts]
 router.post('/', (req, res) => {
   const {topic, text} = req.body;
   posts.push({ id: new Date().getTime().toString(), topic, text });
   res.json({status: 'success'});
 })
 
+// PUT /api/posts/123 => [newPost, ...posts]
 router.put('/:id', (req, res) => {
-    
+  const {topic, text} = req.body;
+
+  posts.forEach(post => {
+    if (post.id === req.params.id) {
+      post.topic = topic
+      post.text = text
+    }
+  })
+  res.json({status: 'success'});
 })
 
+// DELETE /api/posts/123 => [posts without post id]
 router.delete('/:id', (req, res) => {
-    
+  posts = posts.filter(it => it.id !== req.params.id)
+  res.json({status: 'success'});
 })
 
 module.exports = {postsRouter: router}
